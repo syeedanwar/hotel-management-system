535785f44ff559cf813bc4709a978d715b7ebbee
syeedanwar <syeed7015@gmail.com>
2026-07-25 11:16:02 +0530

    Update cancel_booking.php

--- cancel_booking.php
+++ cancel_booking.php
@@ -4,17 +4,24 @@ include 'db.php';
 
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
-    exit();
+    exit;
 }
 
 if (isset($_GET['id'])) {
+
     $id = intval($_GET['id']);
 
-    // Delete booking
-    mysqli_query($conn, "DELETE FROM bookings WHERE id = $id");
+    $stmt = $conn->prepare("DELETE FROM bookings WHERE id = ?");
+    $stmt->bind_param("i", $id);
 
-    // Redirect back
-    header("Location: admin_dashboard.php");
-    exit();
+    if ($stmt->execute()) {
+        header("Location: dashboard.php?msg=cancelled");
+    } else {
+        echo "Unable to cancel booking.";
+    }
+
+    $stmt->close();
 }
+
+$conn->close();
 ?>
